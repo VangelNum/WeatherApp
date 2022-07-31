c70d1cb3aa7d6fc51b08ad087414994d5e9c82d2
VangelNum <vangelnum@gmail.com>
2022-07-31 21:59:07 +0400

    commit

---
+++ app/src/main/java/com/empty/weatherapp/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.empty.weatherapp
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+
+    val City: String = "Izhevsk"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getCurrentLocation()
+
+
+    }
+    private fun getCurrentLocation()
+    {
+        if (checkPermissions())
+        {
+            if (isLocationEnabled())
+            {
+                //latitude and logitude code
+                    fusedLocationProviderClient.lastLocation.addOnCompleteListener(this) {
+                        task->val location: Location? =task.result
+                        if (location == null) {
+                          Toast.makeText(this,"null",Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            Toast.makeText(this,"get success",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    )!== PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission())
+            }
+            else
+            {
+                requestPermission()
+                Toast.makeText(this,"Turn on location",Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                //setting open here
+            }
+        }
+        else {
+
+
+            //request permission here
+        }
+    }
+
+    private fun isLocationEnabled() : Boolean {
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION)
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode== PERMISSION_REQUEST_ACCESS_LOCATION)
+        {
+            if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(applicationContext,"Granted",Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            }
+            else {
+                Toast.makeText(applicationContext,"Denied",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkPermissions(): Boolean
+    {
+        if (ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+            {
+                return true
+            }
+        return false
+    }
+
+}
